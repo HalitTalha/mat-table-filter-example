754e65dce7e244e9a490e1eab0fb8f31ca730bc7
Talha <halit.talha.ture@gmail.com>
2019-09-03 00:16:44 +0300

    Initial commit
    Demonstrates mat-table-filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { MatTableFilter } from './../../../mat-table-extensions/projects/mat-table-filter/src/lib/mat-table-filter.enum';
+import { Component, OnInit } from '@angular/core';
+
+export class Captain {
+  name: string;
+  surname: string;
+}
+
+export class SpaceCraft {
+  name: string;
+  isConstitutionClass: boolean;
+  captain: Captain;
+}
+
+const SPACECRAFT_DATA: SpaceCraft[] = [
+  {name: 'Endurance', isConstitutionClass: false, captain: {name: 'Joseph', surname: 'Cooper'}},
+  {name: 'Enterprise', isConstitutionClass: false, captain: {name: 'Christopher', surname: 'Pike'}},
+  {name: 'Discovery', isConstitutionClass: false, captain: {name: 'Christopher', surname: 'Pike'}},
+  {name: 'Enterprise', isConstitutionClass: false, captain: {name: 'Jean-Luc', surname: 'Pickard'}}
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mat-table-filter-example';
+
+export class AppComponent implements OnInit {
+  filterEntity: SpaceCraft;
+  filterType: MatTableFilter;
+  displayedColumns: string[] = ['name', 'captainName', 'captainSurname', 'isConstitutionClass'];
+  dataSource = new MatTableDataSource(SPACECRAFT_DATA);
+  constructor() { }
+
+  ngOnInit() {
+    // Do not forget to initialize your object and it's non-primitive properties
+    this.filterEntity = new SpaceCraft();
+    this.filterEntity.captain = new Captain();
+    this.filterType = MatTableFilter.ANYWHERE;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { MatTableFilterModule } from 'mat-table-filter';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatTableModule, MatButtonModule, MatPaginatorModule, MatSelectModule, MatFormFieldModule, MatCheckboxModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatTableFilterModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
